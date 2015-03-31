c5d06c5d30b9d6d368225ebaa766716ab22e0195
Alex <acorey07@gmail.com>
2015-03-31 16:06:45 -0700

    Initial Commit

---
+++ Dreamer App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dreamer App
+//
+//  Created by Alex Corey on 3/31/15.
+//  Copyright (c) 2015 El Nuevo Sol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
